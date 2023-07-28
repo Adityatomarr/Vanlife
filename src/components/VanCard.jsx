f0fc307c2ee4e2242435579d53bb6fb4d18ca8d3
Aditya <aditya.tomar.115@gmail.com>
2023-07-28 20:28:30 +0530

    change home page background image

--- src/components/VanCard.jsx
+++ src/components/VanCard.jsx
@@ -9,7 +9,7 @@ export default function VanCard(props){
             <Link to={`/vans/${props.id}`}>
                 <img src={props.image} className="van-card_img"></img>
                 
-                <h2 className="van-card_details"><span>{props.name}</span> {props.price}</h2>
+                <h2 className="van-card_details"><span>{props.name}</span> ${props.price}</h2>
                 <p className="van-card_duration">/day</p>
                 <div className={`van-card_type ${props.type}`}>{props.type}</div>
             </Link>
